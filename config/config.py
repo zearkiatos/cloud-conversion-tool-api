352198e8f399f99878261f68490b2abba5a5b140
zearkiatos <caprilespe@outlook.com>
2023-10-20 18:44:12 -0300

    Fix some issues

--- config/config.py
+++ config/config.py
@@ -1,11 +1,16 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv(dotenv_path='.env')
+environment = os.getenv('FLASK_ENV')
+
+if environment == 'develop':
+    load_dotenv(dotenv_path='.env.dev')
+else:
+    load_dotenv(dotenv_path='.env')
 
 
 class Config:
-    ENVIRONMENT = os.getenv('FLASK_ENV')
+    ENVIRONMENT = environment
     REDIS_BROKER_BASE_URL=os.getenv('REDIS_BROKER_BASE_URL')
     SECRET_KEY=os.getenv('SECRET_KEY')
     JWT_SECRET_KEY=os.getenv('JWT_SECRET_KEY')
